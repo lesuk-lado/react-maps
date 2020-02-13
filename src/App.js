4f89264e8875d935a0170d9c22a0d7d314800731
jaden-lesuk <jadenlesuk12@gmail.com>
2020-02-13 10:51:06 +0300

    Added a .env file to store API key

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ export default function App() {
   return (
     <div style={{ width: '100vw', height: '100vh'}}>
       <WrappedMap 
-        googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places`} 
+        googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=AIzaSyAI5AMG2KrmO9YxzfoDbEz0-5-gjhilc08`} 
         loadingElement={<div style={{ height: "100%" }} />}
         containerElement={<div style={{ height: "100%" }} />}
         mapElement={<div style={{ height: "100%" }} />}
