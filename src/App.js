9376a1e247ed0c155316d0341fef5d572d052ce3
jaden-lesuk <jadenlesuk12@gmail.com>
2020-02-13 11:15:51 +0300

    Removed the .env file and added another API key

--- src/App.js
+++ src/App.js
@@ -12,12 +12,13 @@ function Map(){
 }
 
 const WrappedMap = withScriptjs(withGoogleMap(Map));
+const API_KEY = process.env.REACT_APP_API_KEY;
 
 export default function App() {
   return (
     <div style={{ width: '100vw', height: '100vh'}}>
       <WrappedMap 
-        googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=AIzaSyAI5AMG2KrmO9YxzfoDbEz0-5-gjhilc08`} 
+        googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=AIzaSyDTo4yuHCcyjs6R0qN8U3kak0ey7aGgthQ`}
         loadingElement={<div style={{ height: "100%" }} />}
         containerElement={<div style={{ height: "100%" }} />}
         mapElement={<div style={{ height: "100%" }} />}
