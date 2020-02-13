e393bf827ea24b4c4d24b2d955925a9b72fc7402
jaden-lesuk <jadenlesuk12@gmail.com>
2020-02-13 10:08:24 +0300

    First Iteration of map

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
 import React from 'react';
+import {GoogleMap, withScriptjs, withGoogleMap} from "react-google-maps";
 
-function App() {
-  return <div>map here</div>
+function Map(){
+  return (
+    <GoogleMap 
+    defaultZoom={10} 
+    defaultCenter={{ lat:-1.292066, lng: 36.821945 }}
+    />
+  );
+
 }
 
-export default App;
+const WrappedMap = withScriptjs(withGoogleMap(Map));
+
+export default function App() {
+  return (
+    <div style={{ width: '100vw', height: '100vh'}}>
+      <WrappedMap 
+        googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places`} 
+        loadingElement={<div style={{ height: "100%" }} />}
+        containerElement={<div style={{ height: "100%" }} />}
+        mapElement={<div style={{ height: "100%" }} />}
+      />
+    </div>
+  );
+}
